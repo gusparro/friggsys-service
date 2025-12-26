4c8afd9cf623906439ea517f1395739dbabe6ebf
Gustavo Ferreira Parro <gustavoparro.dev@gmail.com>
2025-12-26 17:15:55 -0300

    feat(adapter): criar classe de configuração inicial de segurança

---
+++ src/main/java/com/gusparro/friggsys/adapter/security/ApplicationSecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.gusparro.friggsys.adapter.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfiguration {
+
+    @Value("${api.prefix}")
+    private String uri;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+
+        return http.build();
+    }
+
+}
