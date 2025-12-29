fb701cdec137ff4fd5c636c2d82922cd83b8586f
Gustavo Ferreira Parro <gustavoparro.dev@gmail.com>
2025-12-29 19:14:39 -0300

    fix(adapter): ajustar nome do pacote de reponse para responde

---
+++ src/main/java/com/gusparro/friggsys/adapter/api/response/HealthCheckResponse.java
@@ -0,0 +1,20 @@
+package com.gusparro.friggsys.adapter.api.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record HealthCheckResponse(
+        @JsonProperty("application_name")
+        String applicationName,
+
+        @JsonProperty("application_version")
+        String version,
+
+        @JsonProperty("java_version")
+        String java,
+
+        @JsonProperty("environment")
+        String environment,
+
+        @JsonProperty("uptime")
+        String uptime
+) {}
